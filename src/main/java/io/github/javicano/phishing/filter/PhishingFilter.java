f2f5796f1e4cc01aeaea9153f11521e24db32d1c
javier.cano <javier.cano@soprasteria.com>
2021-05-22 13:14:57 +0200

    Add model to resources, improve implementation

--- src/main/java/io/github/javicano/phishing/filter/PhishingFilter.java
+++ src/main/java/io/github/javicano/phishing/filter/PhishingFilter.java
@@ -1,5 +1,9 @@
 package io.github.javicano.phishing.filter;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.james.mime4j.MimeException;
 
 /**
  * PhishingFilter
@@ -13,16 +17,21 @@ public interface PhishingFilter {
      *
      * @param email 
      * @return features
+     * @throws StackOverflowError 
+     * @throws IOException 
+     * @throws MimeException 
      */
-	Features getFeatures(String email);
+	Features getFeatures(File email) throws MimeException, IOException, StackOverflowError;
 	
     /**
      * Returns a boolean indicating is email is phishing 
      *
      * @param email 
      * @return isPhishing
+     * @throws MimeException 
+     * @throws StackOverflowError 
      */
-	boolean isPhishingEmail(String email);
+	boolean isPhishingEmail(File email) throws StackOverflowError, MimeException;
 	
 
 }
--- src/main/java/io/github/javicano/phishing/filter/PhishingFilterImpl.java
+++ src/main/java/io/github/javicano/phishing/filter/PhishingFilterImpl.java
@@ -1,6 +1,9 @@
 package io.github.javicano.phishing.filter;
 
+import java.io.File;
+
 import java.io.IOException;
+import java.net.URL;
 
 import org.apache.james.mime4j.MimeException;
 
@@ -17,69 +20,38 @@ class PhishingFilterImpl implements PhishingFilter {
 	}
 
 	@Override
-	public Features getFeatures(String emailPath) {
+	public Features getFeatures(File email) throws MimeException, IOException, StackOverflowError {
 		EmailParser emailParser = new EmailParser();
-		try {
-			return emailParser.getEmailFeatures(emailPath);
-		} catch (StackOverflowError | MimeException | IOException e) {
-			e.printStackTrace();
-		}
-		return new Features();
+		return emailParser.getEmailFeatures(email.getPath());
 	}
 
 	@Override
-	public boolean isPhishingEmail(String email) {
+	public boolean isPhishingEmail(File email) throws StackOverflowError, MimeException {
 		EasyPredictModelWrapper model;
 		try {
-			String path = "/Users/jcano/Documents/phishing-email-detection/PhishingFilterTool/src/main/java/io/github/javicano/phishing/filter/StackedEnsemble_AllModels_AutoML_20210516_182200.zip";
-			model = new EasyPredictModelWrapper(MojoModel.load(path));
+			URL url = getClass().getClassLoader().getResource("./models/StackedEnsemble_AllModels_AutoML_20210516_182200.zip");
+			model = new EasyPredictModelWrapper(MojoModel.load(url.getPath()));
+			
+			Features features = this.getFeatures(email);
 			
 			RowData row = new RowData();
-			row.put("htmlBody", "True");
-			row.put("scriptTag", "True");
-			row.put("hexadecimalURLs", "0");
-			row.put("domainsCount", "7");
-			row.put("dotsCount", "3");
-			row.put("isAccountTerm", "False");
-			row.put("isDearTeam", "False");
-			row.put("imagesAsURL", "121");
-			row.put("ipUrls", "0");
-			row.put("isPayPalTerm", "True");
-			row.put("isLoginTerm", "False");
-			row.put("isBankTerm", "False");
-			row.put("isVerifyTerm", "False");
-			row.put("isAgreeTerm", "False");
-			row.put("isSuspendTerm", "False");
-			row.put("phishingTermsWeight", "2.382271463157895");
-			row.put("domainSender", "False");
-			
-			/*row.put("htmlBody", "True");
-			row.put("scriptTag", "False");
-			row.put("hexadecimalURLs", "0");
-			row.put("domainsCount", "3");
-			row.put("dotsCount", "3");
-			row.put("isAccountTerm", "True");
-			row.put("isDearTeam", "True");
-			row.put("imagesAsURL", "0");
-			row.put("ipUrls", "0");
-			row.put("isPayPalTerm", "True");
-			row.put("isLoginTerm", "False");
-			row.put("isBankTerm", "False");
-			row.put("isVerifyTerm", "True");
-			row.put("isAgreeTerm", "False");
-			row.put("isSuspendTerm", "False");
-			row.put("phishingTermsWeight", "7.6842105157894744");
-			row.put("domainSender", "False");*/
-
-			BinomialModelPrediction p = model.predictBinomial(row);
-			System.out.println("Has penetrated the prostatic capsule (1=yes; 0=no): " + p.label);
-			System.out.print("Class probabilities: ");
-			for (int i = 0; i < p.classProbabilities.length; i++) {
-				if (i > 0) {
-					System.out.print(",");
-				}
-				System.out.print(p.classProbabilities[i]);
+			String[] fieldNames = features.fieldNames();
+			String[] featuresArray = features.toArray();
+			for (int i = 0; i < fieldNames.length; i ++) {
+				row.put(fieldNames[i], featuresArray[i]);
 			}
+			
+			BinomialModelPrediction p = model.predictBinomial(row);	
+			System.out.println("Class probabilities: ");
+			for (int i = 0; i < p.classProbabilities.length; i++) {
+				if (i == 0) {
+					System.out.println("  False:" + p.classProbabilities[i]);
+				} else if ( i == 1) {
+					System.out.println("  True:" + p.classProbabilities[i]);
+				}
+				
+			}
+			System.out.println("Is phishing?: " + p.label);
 			System.out.println("");
 			return p.label.equalsIgnoreCase("1") ? true : false;
 		
